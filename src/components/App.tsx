80dd9e5ac42ef4e74634ae999a2b85c1b4901825
kammanz <kaumilmanzoor@gmail.com>
2023-01-10 14:10:13 -0500

    form resuable for login

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
-import Signup from './Signup';
-import Login from './Login';
-// import Form from './Form';
-import Form3 from './Form3';
+import Form from './Form';
 import UserPage from './UserPage';
 import PrivateRoute from './PrivateRoute';
 
@@ -12,10 +9,10 @@ const App = () => {
   return (
     <Container className="d-flex align-items-center justify-content-center">
       <Routes>
-        <Route path="/" element={<Form3 />} />
+        <Route path="/" element={<Form formType="signup" />} />
       </Routes>
       <Routes>
-        <Route path="/login" element={<Login />} />
+        <Route path="/login" element={<Form formType="login" />} />
       </Routes>
       <Routes>
         <Route element={<PrivateRoute />}>
--- src/components/Login.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Login = () => {
-  return <div>i am a login page</div>;
-};
-
-export default Login;
