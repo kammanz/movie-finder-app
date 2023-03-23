17dc4c319ac909ea3402c07c4791b6fd411987ea
kammanz <kaumilmanzoor@gmail.com>
2023-03-23 15:02:42 -0400

    made it refetch and disabled button now shows

--- src/pages/homepage/MovieList.tsx
+++ src/pages/homepage/MovieList.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { useQueryClient } from 'react-query';
+import React, { useEffect, useState } from 'react';
+import { useQuery, useQueryClient } from 'react-query';
 import { getImgUrl } from '../../api';
 import {
   addToFirestore,
@@ -7,13 +7,56 @@ import {
   updateCachedMovie,
 } from '../../utils/utils';
 import { TClickType, TuserEmail, TMovie, TMovieList } from '../../types';
+import { useAuth } from '../../auth/useAuth';
 
 import styles from './MovieList.module.css';
-import { useMovies } from './hooks';
+import { getUsersSavedMovies, useMovies, addSavedMoviesToList } from './hooks';
+import { queryKeys } from '../../react-query/constants';
 
-const MovieList = ({ userEmail }: TMovieList) => {
+const MovieList = ({ userEmail }: { userEmail: TuserEmail }) => {
+  const [fetchNumber, setFetchNumber] = useState(0);
   const queryClient = useQueryClient();
   const { data: movies } = useMovies();
+  // console.log('movielist, userEmail: ', userEmail);
+  // const userEmail: TuserEmail = localStorage.getItem('user');
+  console.log('movielist, userEmail: ', userEmail);
+
+  // const { user } = useAuth();
+
+  // const userEmail = user?.email;
+
+  const handleSuccess = (savedMovies: TMovie[]) => {
+    console.log('in handle succes');
+    // if (fetchNumber === 0) {
+    //   console.log('in handle succes, fetchNumber === 0');
+    //   setFetchNumber(1);
+    //   refetch();
+    // }
+    const finalMovies = addSavedMoviesToList(movies, savedMovies);
+    queryClient.setQueryData(queryKeys.movies, finalMovies);
+  };
+  const { data: savedMovies, refetch } = useQuery(
+    queryKeys.usersSavedMovies,
+    () => getUsersSavedMovies(userEmail),
+    {
+      enabled: !!movies,
+      refetchOnWindowFocus: false,
+      // refetchInterval: 5000,
+      onSuccess: (savedMovies) => {
+        console.log('on success ran');
+        console.log('on success ran, movies', movies);
+        console.log('on success, savedMovies', savedMovies);
+        if (savedMovies.length > 0) {
+          handleSuccess(savedMovies);
+        }
+      },
+    }
+  );
+
+  useEffect(() => {
+    console.log('use effect ran');
+    setTimeout(() => refetch(), 2000);
+  }, []);
 
   const handleClick = async (movie: TMovie, clickType: TClickType) => {
     const cachedMovies = queryClient.getQueryData<TMovie[]>('movies');
