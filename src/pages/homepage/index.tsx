73f3e94a367094baf942621b9f0c41f79b2c9fcb
kammanz <kaumilmanzoor@gmail.com>
2023-03-24 14:31:29 -0400

    cleanup

--- src/pages/homepage/index.tsx
+++ src/pages/homepage/index.tsx
@@ -1,40 +1,16 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useAuth } from '../../auth/useAuth';
-import { TuserEmail, TMovie, TMovieSortOptions } from '../../types';
+import { TuserEmail } from '../../types';
 import Header from '../../components/header';
 import MovieList from './MovieList';
 
 const Homepage = () => {
-  const [selectedMovieSort, setSelectedMovieSort] =
-    useState<TMovieSortOptions>('newest');
-  const [sortedMovies, setSortedMovies] = useState<TMovie[] | undefined>();
   const { user } = useAuth();
   let userEmail: TuserEmail = user?.email;
-
-  const handleSort = (sortType: TMovieSortOptions) => {
-    setSelectedMovieSort(sortType);
-  };
 
   return (
     <>
       <Header userEmail={userEmail} />
-      {/* <div>
-        <form>
-          <label htmlFor="sort-movies">Sort by:</label>
-          <select
-            id="sort-movies"
-            data-testid="select"
-            value={selectedMovieSort}
-            onChange={(e) => handleSort(e.target.value as TMovieSortOptions)}>
-            <option value="newest">newest</option>
-            <option value="oldest">oldest</option>
-            <option value="thirty-days">last 30 days</option>
-          </select>
-        </form>
-        <button type="button" onClick={() => setSortedMovies(undefined)}>
-          Show all movies
-        </button>
-      </div> */}
       <MovieList userEmail={userEmail} />
     </>
   );
