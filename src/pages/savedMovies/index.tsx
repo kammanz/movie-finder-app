87b93751dc0de5bc3d1ba7e746d6caa42f9d2e09
kammanz <kaumilmanzoor@gmail.com>
2023-04-03 14:32:05 -0400

    render list of saved movies

--- src/pages/savedMovies/index.tsx
+++ src/pages/savedMovies/index.tsx
@@ -1,14 +1,56 @@
 import React from 'react';
 import Header from '../../components/header';
 import { useFullMovies } from '../homepage/hooks';
+import { addToFirestore, removeFromFirestore } from '../../utils/utils';
+import { TMovieSortOptions, TMovie } from '../../types';
+import { useAuth } from '../../auth/useAuth';
+import { getImgUrl } from '../../api';
+import styles from '../homepage/MovieList.module.css';
 
 const SavedMovies = () => {
   // import the hook with saved movies
+
+  const { savedMovies, getFirestoreMovies } = useFullMovies();
+  const { user } = useAuth();
+
+  const handleRemove = async (movie: TMovie) => {
+    await removeFromFirestore(movie, user?.email);
+    await getFirestoreMovies();
+  };
+
+  const handleAdd = async (movie: TMovie) => {
+    await addToFirestore(movie, user?.email);
+    await getFirestoreMovies();
+  };
 
   return (
     <div>
       <Header />
       SavedMovies
+      <ul className={styles.container}>
+        {savedMovies?.length ? (
+          savedMovies.map((movie: TMovie) => (
+            <li key={movie.id} className={styles.card}>
+              <img
+                src={getImgUrl(movie.poster_path)}
+                alt={`${movie.title} poster`}
+              />
+              <h6>{movie.title}</h6>
+              <p>Released: {movie.release_date}</p>
+              <button onClick={() => handleAdd(movie)} disabled={movie.isAdded}>
+                Add
+              </button>
+              <button
+                disabled={!movie.isAdded}
+                onClick={() => handleRemove(movie)}>
+                Remove
+              </button>
+            </li>
+          ))
+        ) : (
+          <p>'Your search returned no results'</p>
+        )}
+      </ul>
     </div>
   );
 };
