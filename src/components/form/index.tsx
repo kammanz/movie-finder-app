b4b5dadbff5abd566bf91387b59f8adae3ac8a3e
kammanz <kaumilmanzoor@gmail.com>
2023-06-01 14:34:45 -0400

    hotfixes

--- src/components/form/index.tsx
+++ src/components/form/index.tsx
@@ -33,7 +33,7 @@ const Form = ({ isSignup }: { isSignup: isSignup }) => {
   const question = isSignup
     ? 'Already have an account?'
     : `Don't have an account?`;
-  const cta = isSignup ? 'Log In' : 'Sign Up';
+  const cta = isSignup ? 'Login' : 'Signup';
   const path = isSignup ? '/login' : '/';
 
   const togglePasswordVisibility = () => {
@@ -75,7 +75,7 @@ const Form = ({ isSignup }: { isSignup: isSignup }) => {
 
   return (
     <div className={styles.container}>
-      <h2 className={styles.title}>{isSignup ? 'Sign Up' : 'Log In'}</h2>
+      <h2 className={styles.title}>{isSignup ? 'Signup' : 'Login'}</h2>
       <form onSubmit={handleSubmit(onSubmit)}>
         <div className={styles.emailContainer}>
           <label htmlFor="email">Email</label>
