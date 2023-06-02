99b58d167ce4b970cffb333af3910fd4c3bb4411
kammanz <kaumilmanzoor@gmail.com>
2023-06-02 13:15:39 -0400

    refactor(dropdown.tsx): add isMobile state
    react-select styles dynamic based on window width

--- src/components/dropdown/dropdownStyles.ts
+++ src/components/dropdown/dropdownStyles.ts
@@ -1,42 +1,35 @@
-const isMobile = window.innerWidth < 768;
-const fontSize = isMobile ? 10 : 16;
-const chevronSize = isMobile ? 14 : 20;
-const controlPadding = isMobile ? 14 : 20;
-
-export const dropdownStyles = {
-  container: (provided: any) => ({
-    ...provided,
-  }),
-  control: (styles: any) => ({
-    ...styles,
-    minWidth: 82,
-    paddingRight: controlPadding,
-    borderRadius: 10,
-    boxShadow: '0px 2px 6px rgba(0, 0, 0, 0.25)',
-    fontSize: fontSize,
-  }),
-  valueContainer: (provided: any) => ({
-    ...provided,
-  }),
-  indicatorsContainer: (provided: any) => ({
-    ...provided,
-  }),
-  indicatorSeparator: (base: any) => ({
-    ...base,
-    display: 'none',
-  }),
-  indicatorContainer: (provided: any) => ({
-    ...provided,
-    width: chevronSize,
-    padding: 0,
-  }),
-  dropdownIndicator: (base: any) => ({
-    ...base,
-    color: 'rgba(0, 0, 0, 0.7)',
-  }),
-  option: (provided: any, state: any) => ({
-    ...provided,
-    backgroundColor: state.isSelected ? '#409DE1' : 'white',
-    fontSize: fontSize,
-  }),
+const getDropdownStyles = (isMobile: boolean) => {
+  return {
+    control: (provided: any, state: any) => ({
+      ...provided,
+      minWidth: '20vw',
+      borderRadius: 10,
+      boxShadow: '0px 2px 6px rgba(0, 0, 0, 0.25)',
+      fontSize: isMobile ? 12 : 16,
+    }),
+    valueContainer: (provided: any) => ({
+      ...provided,
+      textAlign: 'center',
+    }),
+    indicatorSeparator: (provided: any) => ({
+      ...provided,
+      display: 'none',
+    }),
+    indicatorContainer: (provided: any) => ({
+      ...provided,
+      width: isMobile ? 14 : 20,
+      padding: 0,
+    }),
+    dropdownIndicator: (provided: any) => ({
+      ...provided,
+      color: 'rgba(0, 0, 0, 0.7)',
+    }),
+    option: (provided: any, state: any) => ({
+      ...provided,
+      backgroundColor: state.isSelected ? '#409DE1' : 'white',
+      fontSize: isMobile ? 10 : 16,
+    }),
+  };
 };
+
+export default getDropdownStyles;
--- src/components/dropdown/index.tsx
+++ src/components/dropdown/index.tsx
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Select from 'react-select';
 import {
   MovieSortOptions,
   DropdownMenu as DropDownMenuType,
 } from '../../types';
 import { SELECT_MENU_OPTIONS } from '../../constants/selectMenuOptions';
-import { dropdownStyles } from './dropdownStyles';
+import getDropdownStyles from './dropdownStyles';
 import styles from './index.module.css';
 
 const DropdownMenu = ({ onSortChange }: DropDownMenuType) => {
+  const initialState = window.innerWidth < 767;
+  const [isMobile, setIsMobile] = useState(initialState);
+  useEffect(() => {
+    const handleResize = () => {
+      const windowWidth = window.innerWidth;
+      setIsMobile(windowWidth < 767);
+    };
+
+    window.addEventListener('resize', handleResize);
+    handleResize();
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const dropdownStyles = getDropdownStyles(isMobile);
   const handleChange = (selectedOption: any) => {
-    console.log('selectedOption', selectedOption);
     onSortChange(selectedOption.value as MovieSortOptions);
   };
 
