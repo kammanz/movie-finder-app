e81f82788f91477ac51bb744706377a8b5bf1b3c
kammanz <kaumilmanzoor@gmail.com>
2023-06-15 13:59:45 -0400

    cleanup

--- src/components/PrivateRoute.tsx
+++ src/components/PrivateRoute.tsx
@@ -1,18 +1,11 @@
 import React from 'react';
 import { Outlet, Navigate } from 'react-router-dom';
-import { useAuth } from '../auth/useAuth';
 import { getStoredUser } from '../user-storage';
 
 const PrivateRoute = () => {
-  const user2 = getStoredUser();
-  const { user } = useAuth();
-  console.log('user', user);
-  console.log('user2', user2);
-  console.log('user?.isAnonymous', user?.isAnonymous);
+  const user = getStoredUser();
 
-  // console.log('user', user.is);
-
-  return user2 ? <Outlet /> : <Navigate to="/login" />;
+  return user ? <Outlet /> : <Navigate to="/login" />;
 };
 
 export default PrivateRoute;
